3de612a5ceb9864c63e570f5359a01944f8c1474
tafaramafemba <tafara.maf@gmail.com>
2022-04-26 17:18:18 +0200

    add styling

--- to-do-react/src/components/Header.js
+++ to-do-react/src/components/Header.js
@@ -1,9 +1,10 @@
 import React from "react"
+import '../App.css'
 
 const Header = () => {
   return (
     <header>
-      <h1>todos</h1>
+      <h1 className="header-h1">todos</h1>
     </header>
   )
 }
--- to-do-react/src/components/InputTodo.js
+++ to-do-react/src/components/InputTodo.js
@@ -26,13 +26,16 @@ class InputTodo extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input type="text" placeholder="Add todo..." 
-        value={this.state.title} 
-        name="title"
-        onChange={this.onChange}
+      <form onSubmit={this.handleSubmit} className="form-container">
+        <input
+          type="text"
+          className="input-text"
+          placeholder="Add todo..."
+          value={this.state.title}
+          name="title"
+          onChange={this.onChange}
         />
-        <button>Submit</button>
+        <button className="input-submit">Submit</button>
       </form>
     )
   }
